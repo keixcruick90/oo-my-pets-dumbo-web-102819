3f4bc417a3376ebf495f11902f9cb408786f1b20
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-11-25 20:51:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,11 +28,11 @@ class Owner
   end
 
   def cat
-    Cat.all.map { |cat_instance| cat_instance.owner == owner.name}
+    Cat.all.map { |cat_instance| cat_instance.owner == self}
   end
 
   def dog
-    Dog.all.map { |dog_instance| dog_instance.owner == owner.name }
+    Dog.all.map { |dog_instance| dog_instance.owner == self }
   end
 
 end
